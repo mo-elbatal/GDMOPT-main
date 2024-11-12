4dbb2589b1c8dc7ae39a1a0a8b530b16bf577b23
Rashd Fawzy <rashad.fawzy@ejust.edu.eg>
2024-11-12 07:17:18 +0200

    update 12.11

--- ON-OFF-DRL-main/td3_256.py
+++ ON-OFF-DRL-main/td3_256.py
@@ -48,37 +48,38 @@ class ReplayBuffer:
     def __len__(self):
         return len(self.buffer)
     
-# class NormalizedActions(Env.ac):
-#     def _action(self, action):
-#         low  = self.action_space.low
-#         high = self.action_space.high
+class NormalizedActions:
+    def _action(self, action_tens):
+        low  = 0
+        high = action_dim-1
+        action = action_tens.multinomial(1)
+        action = low + (action + 1.0) * 0.5 * (high - low)
+        action = np.clip(action, low, high)
         
-#         action = low + (action + 1.0) * 0.5 * (high - low)
-#         action = np.clip(action, low, high)
-        
-#         return action
+        return action
 
-#     def _reverse_action(self, action):
-#         low  = self.action_space.low
-#         high = self.action_space.high
+    def _reverse_action(self, action_tens):
+        low  = 0
+        high = action_dim-1
+        action = action_tens.multinomial(1)
+        action = 2 * (action - low) / (high - low) - 1
+        action = np.clip(action, low, high)
         
-#         action = 2 * (action - low) / (high - low) - 1
-#         action = np.clip(action, low, high)
-        
-#         return action
+        return action
 
-class GaussianExploration(object):
-    def __init__(self, max_sigma=1.0, min_sigma=1.0, decay_period=1000000):
-        self.low  = 0
-        self.high = env.n_servers
-        self.max_sigma = max_sigma
-        self.min_sigma = min_sigma
-        self.decay_period = decay_period
+# used with continuous action space
+# class GaussianExploration(object):
+#     def __init__(self, max_sigma=1.0, min_sigma=1.0, decay_period=1000000):
+#         self.low  = 0
+#         self.high = action_dim
+#         self.max_sigma = max_sigma
+#         self.min_sigma = min_sigma
+#         self.decay_period = decay_period
     
-    def get_action(self, action, t=0):
-        sigma  = self.max_sigma - (self.max_sigma - self.min_sigma) * min(1.0, t / self.decay_period)
-        action = action + np.random.normal(size=len(action)) * sigma
-        return np.clip(action, self.low, self.high)
+#     def get_action(self, action, t=0):
+#         sigma  = self.max_sigma - (self.max_sigma - self.min_sigma) * min(1.0, t / self.decay_period)
+#         action = action + np.random.normal(size=len(action)) * sigma
+#         return np.clip(action, self.low, self.high)
 
 def soft_update(net, target_net, soft_tau=1e-2):
     for target_param, param in zip(target_net.parameters(), net.parameters()):
@@ -86,85 +87,95 @@ def soft_update(net, target_net, soft_tau=1e-2):
                 target_param.data * (1.0 - soft_tau) + param.data * soft_tau
             )
 
-
 class PolicyNetwork(nn.Module):
-    def __init__(self, num_inputs, num_actions, hidden_size, init_w=3e-3):
+    def __init__(self, num_inputs, num_actions, hidden_size):
         super(PolicyNetwork, self).__init__()
         
         self.linear1 = nn.Linear(num_inputs, hidden_size)
         self.linear2 = nn.Linear(hidden_size, hidden_size)
         self.linear3 = nn.Linear(hidden_size, num_actions)
-        
-        self.linear3.weight.data.randn(-init_w, init_w)
-        self.linear3.bias.data.uniform_(-init_w, init_w)
+        self.softmax = nn.Softmax(dim=1)
         
     def forward(self, state):
         x = F.relu(self.linear1(state))
         x = F.relu(self.linear2(x))
-        x = F.tanh(self.linear3(x))
+        x = F.relu(self.linear3(x))
+        # x = tanh(self.linear3(x)  # tanh function used for continuous-action space values [-1, 1] - interferes with probs
+        x = self.softmax(x)
         return x
     
     def get_action(self, state):
         state  = torch.FloatTensor(state).unsqueeze(0).to(device)
         action = self.forward(state)
-        return action.detach().cpu().numpy()[0]
+        return action
+        # return action.detach().cpu().numpy()[0]
 
 class ValueNetwork(nn.Module):
-    def __init__(self, num_inputs, num_actions, hidden_size, init_w=3e-3):
+    def __init__(self, num_inputs, num_actions, hidden_size):
         super(ValueNetwork, self).__init__()
         
         self.linear1 = nn.Linear(num_inputs + num_actions, hidden_size)
         self.linear2 = nn.Linear(hidden_size, hidden_size)
         self.linear3 = nn.Linear(hidden_size, 1)
         
-        self.linear3.weight.data.uniform_(-init_w, init_w)
-        self.linear3.bias.data.uniform_(-init_w, init_w)
-        
     def forward(self, state, action):
+        action = F.one_hot(action, num_classes=action_dim).int()  # One-hot encode the action
         x = torch.cat([state, action], 1)
         x = F.relu(self.linear1(x))
         x = F.relu(self.linear2(x))
         x = self.linear3(x)
-        return x
-    
-## Step update in TD3 
-target_q_value = None
-expected_q_value = None
 
 def td3_update(step,
-           batch_size,
-           gamma = 0.99,
-           soft_tau=1e-2,
-           noise_std = 0.2,
-           noise_clip=0.5,
-           policy_update=2,
-          ):
+                batch_size,
+                gamma=0.99,
+                soft_tau=1e-2,
+                noise_std=0.2,
+                noise_clip=0.5,
+                policy_update=2):
 
     state, action, reward, next_state, done = replay_buffer.sample(batch_size)
 
-    state      = torch.FloatTensor(state).to(device)
+    # Convert all to tensors and move to device
+    state = torch.FloatTensor(state).to(device)
     next_state = torch.FloatTensor(next_state).to(device)
-    action     = torch.FloatTensor(action).to(device)
-    reward     = torch.FloatTensor(reward).unsqueeze(1).to(device)
-    done       = torch.FloatTensor(np.float32(done)).unsqueeze(1).to(device)
+    action = torch.LongTensor(action).to(device)  # For discrete actions, use LongTensor
+    reward = torch.FloatTensor(reward).unsqueeze(1).to(device)
+    done = torch.FloatTensor(np.float32(done)).unsqueeze(1).to(device)
+    print("action is: ", action)
 
-    next_action = target_policy_net(next_state)
-    noise = torch.normal(torch.zeros(next_action.size()), noise_std).to(device)
+    # Policy network predicts probabilities for each action
+    next_action_probs = target_policy_net(next_state)
+    next_action_dist = torch.distributions.Categorical(probs=next_action_probs)
+    next_action = next_action_dist.sample()
+    print("next action is: ", next_action)
+
+    # Add noise to target actions for exploration, clipped within limits
+    noise = torch.normal(0, noise_std, size=next_action.shape).to(device)
     noise = torch.clamp(noise, -noise_clip, noise_clip)
-    next_action += noise
+    next_action = torch.clamp(next_action + noise, 0, action_dim - 1).long()
 
-    target_q_value1  = target_value_net1(next_state, next_action)
-    target_q_value2  = target_value_net2(next_state, next_action)
-    target_q_value   = torch.min(target_q_value1, target_q_value2)
+    # Get Q-value estimates from target networks, selecting based on `next_action`
+    print("next action is: ", next_action)
+    print("next action dim is: ", next_action.shape)
+    print("next state is: ", next_state)
+    print("next action dim is: ", next_state.shape)
 
+    target_q_value1 = target_value_net1(next_state, next_action)
+    target_q_value2 = target_value_net2(next_state, next_action)
+    target_q_value = torch.min(target_q_value1, target_q_value2)
+    
+    # Calculate the expected Q-value
     expected_q_value = reward + (1.0 - done) * gamma * target_q_value
 
+    # Calculate Q-values for the current state and chosen action
     q_value1 = value_net1(state, action)
     q_value2 = value_net2(state, action)
 
-    value_loss1 = value_criterion(q_value1, expected_q_value.detach())
-    value_loss2 = value_criterion(q_value2, expected_q_value.detach())
+    # Calculate losses for the value networks
+    value_loss1 = F.mse_loss(q_value1, expected_q_value.detach())
+    value_loss2 = F.mse_loss(q_value2, expected_q_value.detach())
 
+    # Optimize the value networks
     value_optimizer1.zero_grad()
     value_loss1.backward()
     value_optimizer1.step()
@@ -173,17 +184,26 @@ def td3_update(step,
     value_loss2.backward()
     value_optimizer2.step()
 
+    # Policy update at intervals
     if step % policy_update == 0:
-        policy_loss = value_net1(state, policy_net(state))
-        policy_loss = -policy_loss.mean()
+        # Compute policy loss using action values from value_net1
+        action_probs = policy_net(state)
+        action_dist = torch.distributions.Categorical(probs=action_probs)
+        chosen_action = action_dist.sample()
+        
+        policy_loss = -value_net1(state, action).mean()
 
+        # Optimize the policy network
         policy_optimizer.zero_grad()
         policy_loss.backward()
         policy_optimizer.step()
 
+        # Soft update for target networks
         soft_update(value_net1, target_value_net1, soft_tau=soft_tau)
         soft_update(value_net2, target_value_net2, soft_tau=soft_tau)
         soft_update(policy_net, target_policy_net, soft_tau=soft_tau)
+    
+    return target_q_value, expected_q_value
 
 ################################# End of Part I ################################
 
@@ -197,21 +217,20 @@ replay_buffer_size = 1000000
 lr_actor = 0.0003       # learning rate for actor network
 lr_critic = 0.001       # learning rate for critic network
 max_training_timesteps = 100000   # break from training loop if timeteps > max_training_timesteps
-max_ep_len = 225            # max timesteps in one episode
+max_ep_len = 100            # max timesteps in one episode 225
 frame_idx   = 0
 rewards     = []
 batch_size  = 128
 random_seed = 0      
 gamma = 0.99               
 
-random_seed = 0       # set random seed
 print_freq = max_ep_len * 4     # print avg reward in the interval (in num timesteps)
 log_freq = max_ep_len * 2       # saving avg reward in the interval (in num timesteps)
 save_model_freq = max_ep_len * 4         # save model frequency (in num timesteps)
 capacity = 10000
 
 env = Env()
-noise = GaussianExploration()
+# noise = GaussianExploration() for continuous action spaces
 
 # state space dimension
 state_dim = args.n_servers * args.n_resources + args.n_resources + 1
@@ -352,13 +371,13 @@ print_running_episodes = 0
 log_running_reward = 0
 log_running_episodes = 0
 
-frame_idx = 0
+time_step = 0
 i_episode = 0
 num_steps    = 50
 log_interval = 10
 
 
-while frame_idx < max_ep_len:
+while time_step <= max_training_timesteps:
     print("New training episode:")
     sleep(0.1) # we sleep to read the reward in console
     state = env.reset()
@@ -369,45 +388,49 @@ while frame_idx < max_ep_len:
     actions  = []
     rewards  = []
     masks    = []
-    
-    for step in range(max_training_timesteps):
+    normalized_actions = NormalizedActions()
 
-        action = policy_net.get_action(state)
-        action = noise.get_action(action, step)
-        action = np.random.choice(action, size=1, replace=False)
-        next_state, reward, done, _ = env.step(action.item())
+    target_q_value, expected_q_value = 0.0, 0.0
+
+    for step in range(max_ep_len):
+
+        action_tens = policy_net.get_action(state)
+        # action_arr = noise.get_action(action_arr, step)  # for continuous action spaces
+        action = normalized_actions._action(action_tens)
+        action = int(action.item())
+        next_state, reward, done, _ = env.step(action)
         
         state = next_state
         current_ep_reward += reward
-        frame_idx += 1
-        print("The current total episodic reward at timestep:", frame_idx, "is:", current_ep_reward)
+        time_step += 1
+        print("The current total episodic reward at timestep:", time_step, "is:", current_ep_reward)
         sleep(0.1) # we sleep to read the reward in console
         
-        policy = PolicyNetwork(state)
         reward = torch.FloatTensor([reward]).unsqueeze(1).to(device)
         mask   = torch.FloatTensor(1 - np.float32([done])).unsqueeze(1).to(device)
 
         replay_buffer.push(state, action, reward, next_state, done)
+
         if len(replay_buffer) > batch_size:
-            td3_update(step, batch_size)
+            target_q_value, expected_q_value = td3_update(step, batch_size)
 
         q_values.append(target_q_value)
-        policies.append(policy)
+        policies.append(target_policy_net)
         actions.append(action)
         rewards.append(reward)
         values.append(expected_q_value)
         masks.append(mask)
 
         # log in logging file
-        if frame_idx % log_freq == 0:
+        if time_step % log_freq == 0:
 
             # log average reward till last episode
             log_avg_reward = log_running_reward / log_running_episodes
             log_avg_reward = round(log_avg_reward, 4)
 
-            log_f.write('{},{},{}\n'.format(i_episode, frame_idx, log_avg_reward))
+            log_f.write('{},{},{}\n'.format(i_episode, time_step, log_avg_reward))
             log_f.flush()
-            log_f2.write('{},{},{}\n'.format(i_episode, frame_idx, log_avg_reward))
+            log_f2.write('{},{},{}\n'.format(i_episode, time_step, log_avg_reward))
             log_f2.flush()
             print("Saving reward to csv file")
             sleep(0.1) # we sleep to read the reward in console
@@ -415,19 +438,19 @@ while frame_idx < max_ep_len:
             log_running_episodes = 0
             
         # printing average reward
-        if frame_idx % print_freq == 0:
+        if time_step % print_freq == 0:
 
             # print average reward till last episode
             print_avg_reward = print_running_reward / print_running_episodes
             print_avg_reward = round(print_avg_reward, 2)
             
-            print("Episode : {} \t\t Timestep : {} \t\t Average Reward : {}".format(i_episode, frame_idx, print_avg_reward))
+            print("Episode : {} \t\t Timestep : {} \t\t Average Reward : {}".format(i_episode, time_step, print_avg_reward))
             sleep(0.1) # we sleep to read the reward in console
             print_running_reward = 0
             print_running_episodes = 0
             
         # save model weights
-        if frame_idx % save_model_freq == 0:
+        if time_step % save_model_freq == 0:
             print("--------------------------------------------------------------------------------------------")
             print("saving model at : " + checkpoint_path)
             sleep(0.1) # we sleep to read the reward in console
@@ -442,16 +465,16 @@ while frame_idx < max_ep_len:
         if done:
             break
 
-        next_state = torch.FloatTensor(state).unsqueeze(0).to(device)     
-        
-        print_running_reward += current_ep_reward
-        print_running_episodes += 1
+    next_state = torch.FloatTensor(state).unsqueeze(0).to(device)     
+    
+    print_running_reward += current_ep_reward
+    print_running_episodes += 1
 
-        log_running_reward += current_ep_reward
-        log_running_episodes += 1
+    log_running_reward += current_ep_reward
+    log_running_episodes += 1
 
-        i_episode += 1
-        frame_idx += num_steps
+    i_episode += 1
+    time_step += num_steps
 
 
 log_f.close()
